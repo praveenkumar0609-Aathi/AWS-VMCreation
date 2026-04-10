1f98d25982bb44c8c6686c714493d839c5f15761
praveenkumar0609-Aathi <praveenkumar0609@gmail.com>
2026-04-10 19:20:03 +0530

    Add S3 bucket and DynamoDB table resources

---
+++ provider.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "tf_bucket" {
+  bucket = "my-terraform-state-bucket"
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
